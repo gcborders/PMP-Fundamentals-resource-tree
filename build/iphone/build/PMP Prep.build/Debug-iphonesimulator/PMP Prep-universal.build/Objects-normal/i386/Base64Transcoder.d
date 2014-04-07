f02096cf883352c6b1def4f2990ee71e18d732ee
gcborders <gregborders@msn.com>
2014-04-06 19:23:30 -0500

    Commit and clean 4/6/14

--- build/iphone/build/PMP Prep.build/Debug-iphonesimulator/PMP Prep-universal.build/Objects-normal/i386/Base64Transcoder.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/oracledba/Documents/Titanium_Studio_Workspace/PMP-Fundamentals-resource-tree/build/iphone/Classes/Base64Transcoder.c \
-  /Users/oracledba/Documents/Titanium_Studio_Workspace/PMP-Fundamentals-resource-tree/build/iphone/Classes/Base64Transcoder.h
