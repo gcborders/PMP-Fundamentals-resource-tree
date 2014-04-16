7d4f36d79704ffc9910a665141a7d7a5bb6b29a6
James Geraci <jdgeraci@gmail.com>
2014-04-16 14:44:13 -0400

    Clean

--- build/iphone/build/PMP Prep.build/Debug-iphonesimulator/PMP Prep-universal.build/Objects-normal/i386/CAStreamBasicDescription.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/oracledba/Documents/Titanium_Studio_Workspace/PMP-Fundamentals-resource-tree/build/iphone/Classes/CAStreamBasicDescription.cpp \
-  /Users/oracledba/Documents/Titanium_Studio_Workspace/PMP-Fundamentals-resource-tree/build/iphone/Classes/CAStreamBasicDescription.h \
-  /Users/oracledba/Documents/Titanium_Studio_Workspace/PMP-Fundamentals-resource-tree/build/iphone/Classes/CADebugMacros.h \
-  /Users/oracledba/Documents/Titanium_Studio_Workspace/PMP-Fundamentals-resource-tree/build/iphone/Classes/CAMath.h
--- build/iphone/build/PMP Prep.build/Debug-iphonesimulator/PMP Prep-universal.build/Objects-normal/i386/Reachability.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/oracledba/Documents/Titanium_Studio_Workspace/PMP-Fundamentals-resource-tree/build/iphone/Classes/ASI/Reachability.m \
-  /Users/oracledba/Documents/Titanium_Studio_Workspace/PMP-Fundamentals-resource-tree/build/iphone/Classes/ASI/Reachability.h
