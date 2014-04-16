5834f093dfb9b6b8b854687f5df81d25d1540029
James Geraci <jdgeraci@gmail.com>
2014-04-16 16:39:46 -0400

    Cleaned Branch
    Clean branch

---
+++ Resources/views/DetailView.js
@@ -0,0 +1,54 @@
+function DetailView(params){
+	var win = Ti.UI.createWindow({
+		title: params.detailTitle,
+		backgroundColor: "#660000",
+		fullscreen: false,	//[Android: makes the window a "heavyweight" window (thereby allowing the back button to work with it)]
+		//exitOnClose: true, //[Android: make the application exit if the back button is pressed from the main window]
+		navBarHidden: false,
+		tabBarHidden: false
+	});
+		
+	var img = Ti.UI.createImageView({
+		image: params.mainImage,
+		width: "100%",
+		height: "180dp",
+		top: (Ti.Platform.osname === "android") ? "0dp" : "0dp",
+	});
+	
+	var detail = Ti.UI.createWebView({
+		url: params.detail,
+		top: (Ti.Platform.osname === "android") ? "180dp" : "180dp",
+		height: Ti.UI.FILL,
+		enableZoomControls: false, //Android only
+    borderRadius:1
+	});
+
+    var os = Ti.Platform.osname;
+	
+	if (os === 'ipad' || os === 'iphone') {
+		var bb = Ti.UI.createButton({ systemButton: Ti.UI.iPhone.SystemButton.BACK, 
+			 backgroundImage:"go_previous_black.png",
+			 touchEnabled: 'True',
+			 borderRadius:'20',
+			 backgroundColor:'gray',
+			 borderColor: 'white',
+			 borderWidth: '1.0',
+			 top: 20,
+			 right: 20,
+			 width: 43,
+			 height: 43
+		});
+		
+		//win.add(bb);
+		bb.addEventListener('click', function(){	
+	    	win.close(win);
+		});
+	}
+	win.add(img);
+	//Add Button After Image
+	if (os === 'ipad' || os === 'iphone'){win.add(bb);}
+	win.add(detail);
+
+	return win;
+}
+module.exports = DetailView;
